8c638097b4cf09e4e33f1566ef5d597daa616758
Ismail-tony <ibabalola21@outlook.com>
2024-01-13 00:34:58 +0000

    changed the const variable to let as  i am dealing with two sets of datas

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const name = 'Mark'
-const height = 1.69
-const weight = 78
-const Mark = weight / height ** 2
-console.log(Mark);
+//Mark's Data
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn =1.95;
+
+let BMIMark = massMark/(heightMark * heightMark);
+let BMIJohn = massJohn/(heightJohn * heightJohn);
+
+console.log(BMIMark, BMIJohn);
+
+const MarkHigherBmi = (BMIMark > BMIJohn);
+console.log(MarkHigherBmi);
+
+massMark = 95;
+heightMark = 1.88;
+massJohn = 85;
+heightJohn
