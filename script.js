54c287a9a330d64cedb09aee45e1263eaabbf5d2
Ismail-tony <ibabalola21@outlook.com>
2024-01-13 10:53:04 +0000

    compiled results in fewer console log

--- script.js
+++ script.js
@@ -6,11 +6,9 @@ let heightJohn =1.95;
 
 let BMIMark = massMark/(heightMark * heightMark);//BMI = 27.3
 let BMIJohn = massJohn/(heightJohn * heightJohn);//BMI = 24.1
-
-console.log(BMIMark, BMIJohn);
-
 let MarkHigherBmi = (BMIMark > BMIJohn); //TRUE
-console.log(MarkHigherBmi);
+console.log(BMIMark, BMIJohn, MarkHigherBmi);
+
 
 //Test Data 2
 massMark = 95;
@@ -20,7 +18,5 @@ heightJohn = 1.76;
 
 BMIMark = massMark/heightMark ** 2; //BMI = 26.8
 BMIJohn = massJohn/heightJohn ** 2;//BMI = 27.4
-console.log(BMIMark, BMIJohn);
-
 MarkHigherBmi = (BMIMark>BMIJohn); //FALSE
-console.log(MarkHigherBmi);
+console.log(BMIMark, BMIJohn, MarkHigherBmi);
